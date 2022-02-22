fc5b25a1cc9152038e35642b6dfb7ca6d52d41d4
Bakare Abiola <murphybiola007@gmail.com>
2022-02-22 12:52:09 +0100

    feat(index.html|index.js) - completed the ui - implemented persisting dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 module.exports = {
+  mode : 'jit',
+  darkMode :'class',
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       colors:{
-        primary: "#0B0A1B"
+        primary: "#0B0A1B",
+        "i-border":"#E9ECF0",
+        'i-border-2':"#122239",
+        muted: '#8C98AD',
+        secondary: '#91A2CB'
       }
     },
   },
